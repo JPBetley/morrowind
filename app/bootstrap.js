2448d65ac972f810af2c00cd85dd84dc29ba9aad
John Betley <jpbetley@gmail.com>
2015-12-25 12:16:23 -0500

    rearranged express app code

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -5,12 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = {
+  index: require('./routes/index'),
+};
 
 var app = express();
 
 // view engine setup
+console.log(__dirname);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -20,16 +22,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes.index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
