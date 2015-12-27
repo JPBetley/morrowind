38f0fcf7067b64a8a8d5d6aa0d4524a5a8159a17
John Betley <jpbetley@gmail.com>
2015-12-27 18:47:08 -0500

    added specializations

---
+++ resources/assets/js/specializations/all.js
@@ -0,0 +1,6 @@
+module.exports = [
+	{ text : require('./default').name, value : require('./default') },
+	{ text : require('./combat').name, value : require('./combat') },
+	{ text : require('./magic').name, value : require('./magic') },
+	{ text : require('./stealth').name, value : require('./stealth') },
+];
